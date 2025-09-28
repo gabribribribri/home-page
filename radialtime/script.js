6479613b1b518e3f2f98b18a1db020ee9c8e01b2
gabribribribri <fuelanemo149@gmail.com>
2025-09-28 20:56:38 +0200

    radialTime is finished

--- radialtime/script.js
+++ radialtime/script.js
@@ -7,37 +7,74 @@ function getMinutesSinceMidnight() {
 }
 
 function gcd(a, b) {
-    if (b === 0) {
-        return a;
-    }
-    return gcd(b, a % b);
+	if (b === 0) {
+		return a;
+	}
+	return gcd(b, a % b);
 }
 
-function getTextFromMinutes(minutes) {
+function getTextFromMinutesGgt(minutes) {
+	let text;
+
+	if (minutes === 3 * 60) {
+		text = "0"
+	}
+
 	let minutes_gcd = Math.abs(gcd(minutes, 30));
 
 	if (minutes_gcd === 30) {
-		return `${minutes/minutes_gcd}\\pi\\hspace{2mm}Ggt`;
+		text = `${minutes / minutes_gcd}\\pi`;
 	} else {
-		return `\\frac{${minutes/minutes_gcd}\\pi}{${30/minutes_gcd}}\\hspace{2mm}Ggt`
+		text = `\\frac{${minutes / minutes_gcd}\\pi}{${30 / minutes_gcd}}`
 	}
+	return text + "\\hspace{2mm}Ggt";
 }
 
+function getTextFromMinutesCt(minutes) {
+	let text;
 
-let minutes = getMinutesSinceMidnight();
+	if (minutes === 0) {
+		text = "0"
+	}
 
-if (minutes < 3 * 60) {
-	minutes += 21 * 60;
-} else if (minutes > 12 * 60) {
-	minutes -= 27 * 60;
-} else {
-	minutes -= 3 * 60;
+	let minutes_gcd = Math.abs(gcd(minutes, 360));
+
+	if (minutes_gcd == 360) {
+		text = `${minutes / minutes_gcd}\\pi`;
+	} else {
+		text = `\\frac{${minutes / minutes_gcd}\\pi}{${360 / minutes_gcd}}`;
+	}
+
+	return text + "\\hspace{2mm}Ct";
 }
 
-let timeText = getTextFromMinutes(minutes);
+function updateRadialTimeGgt() {
+	let minutes = getMinutesSinceMidnight();
+	if (minutes < 3 * 60) {
+		minutes += 21 * 60;
+	} else if (minutes > 12 * 60) {
+		minutes -= 27 * 60;
+	} else {
+		minutes -= 3 * 60;
+	}
+	let timeText = getTextFromMinutesGgt(minutes);
+	katex.render(timeText, document.getElementById("radial-time-ggt"))
+}
+
+function updateRadialTimeCt() {
+	let minutes = getMinutesSinceMidnight();
+
+	if (minutes >= 12 * 60) {
+		minutes -= 24 * 60
+	}
+
+	let timeText = getTextFromMinutesCt(minutes);
+	katex.render(timeText, document.getElementById("radial-time-ct"))
+}
 
 
 document.addEventListener('DOMContentLoaded', function() {
-	katex.render(timeText, document.getElementById("radian-time"));
+	setInterval(updateRadialTimeGgt, 1000)
+	setInterval(updateRadialTimeCt, 1000)
 })
 
